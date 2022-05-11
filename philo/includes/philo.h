73a1db80fb6ea4e22eee3719f2d8dfa35634c14d
AOhapkin <alexeyyago@gmail.com>
2022-05-11 16:57:22 +0300

    fix structs

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: gmyriah <gmyriah@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 09:36:19 by gmyriah           #+#    #+#             */
-/*   Updated: 2022/05/01 09:36:21 by gmyriah          ###   ########.fr       */
+/*   Updated: 2022/05/11 16:57:04 by gmyriah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,32 +23,34 @@
 # define SUCCESS	0
 # define FAIL		1
 
-typedef struct s_philo
+typedef struct s_phil
 {
-	int				n;
-	int				num_of_eat;
-	pthread_mutex_t	*left;
-	pthread_mutex_t	*right;
-	pthread_mutex_t	check_mutex;
-	pthread_t		thread;
-	struct s_info	*info;
-	struct timeval	last_time_to_eat;
-}					t_philo;
+	int					phil_id;
+	int					total_phil;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					num_eat;
+	int					left_fork_id;
+	int					right_fork_id;
+	long long			last_meal_ms;
+	struct s_data		*ptr_data;
+}	t_phil;
 
-typedef struct s_info
+typedef struct s_data
 {
-	int				num_of_philo;
-	int				num_of_eat_finish_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				num_of_must_eat;
-	int				finish;
-	pthread_mutex_t	finish_mutex;
-	t_philo			*philos;
-	pthread_mutex_t	*forks;
-	struct timeval	create_at;
-}					t_info;
+	int					total_phil;
+	int					time_to_die;
+	int					total_eat;
+	int					all_have_eaten;
+	int					end;
+	long long			start_ms;
+	pthread_mutex_t		mutex_print;
+	pthread_mutex_t		mutex_time;
+	pthread_mutex_t		mutex_end;
+	pthread_mutex_t		mutex_all_have_eaten;
+	pthread_mutex_t		*mutex_forks;
+	t_phil				*phil;
+}	t_data;
 
 int			init_simulation(t_info *info, int argc, char *argv[]);
 void		*run_philo_actions(void *argv);
