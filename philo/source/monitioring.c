13288885184a57130bf670b89fc1777aa61d9352
AOhapkin <alexeyyago@gmail.com>
2022-05-11 17:02:49 +0300

    new monitoring

--- philo/source/monitioring.c
+++ philo/source/monitioring.c
@@ -6,50 +6,95 @@
 /*   By: gmyriah <gmyriah@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 09:36:22 by gmyriah           #+#    #+#             */
-/*   Updated: 2022/05/01 09:36:24 by gmyriah          ###   ########.fr       */
+/*   Updated: 2022/05/11 17:02:31 by gmyriah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*monitor_each_must_eat(void *argv)
+int	init_data(int **args, t_data *data)
 {
-	t_info	*info;
-
-	info = argv;
-	while (!info->finish)
-	{
-		pthread_mutex_lock(&info->finish_mutex);
-		if (info->num_of_eat_finish_philo == info->num_of_philo)
-			info->finish = 1;
-		pthread_mutex_unlock(&info->finish_mutex);
-	}
-	return (NULL);
+	data->total_phil = (*args)[0];
+	data->time_to_die = (*args)[1];
+	data->total_eat = (*args)[4];
+	data->end = -1;
+	data->phil = malloc (sizeof(t_phil) * data->total_phil);
+	if (!data->phil)
+		return (show_error_message("Malloc error!\n"));
+	if (set_current_time(&(data->start_ms)))
+		return (1);
+	data->all_have_eaten = 0;
+	init_all_philos(args, data);
+	return (0);
 }
 
-void	*monitor(void *argv)
+long long	set_time_stamp(t_phil *phil, long long	*time_from_start)
 {
-	t_philo			*philo;
-	struct timeval	now;
-	long long		ms;
+	long long	now_ms;
 
-	philo = argv;
-	while (!philo->info->finish)
+	if (set_current_time(&now_ms))
+		return (FAIL);
+	*time_from_start = now_ms - phil->ptr_data->start_ms;
+	return (SUCCESS);
+}
+
+static int	check_all_have_eaten(t_data *data)
+{
+	pthread_mutex_lock(&(data->mutex_all_have_eaten));
+	if (data->all_have_eaten == data->total_phil)
 	{
-		pthread_mutex_lock(&philo->check_mutex);
-		pthread_mutex_lock(&philo->info->finish_mutex);
-		gettimeofday(&now, NULL);
-		ms = time_to_ms(now) - time_to_ms(philo->last_time_to_eat);
-		gettimeofday(&now, NULL);
-		if (ms >= philo->info->time_to_die && philo->info->finish == 0)
-		{
-			printf("%lld\t%d\t %s\n", \
-				time_to_ms(now) - time_to_ms(philo->info->create_at), \
-				philo->n + 1, "died");
-			philo->info->finish = 1;
-		}
-		pthread_mutex_unlock(&philo->info->finish_mutex);
-		pthread_mutex_unlock(&philo->check_mutex);
+		pthread_mutex_unlock(&(data->mutex_all_have_eaten));
+		pthread_mutex_lock(&(data->mutex_end));
+		data->end = 0;
+		pthread_mutex_unlock(&(data->mutex_end));
+		return (1);
 	}
-	return (NULL);
+	else
+		pthread_mutex_unlock(&(data->mutex_all_have_eaten));
+	return (0);
+}
+
+static int	check_time_to_die(t_data *data, int i)
+{
+	long long	now_ms;
+	long long	time_from_start;
+
+	if (set_current_time(&now_ms))
+		return (FAIL);
+	pthread_mutex_lock(&(data->mutex_time));
+	if ((now_ms - data->phil[i].last_meal_ms) > \
+		((long long)data->time_to_die))
+	{
+		pthread_mutex_unlock(&(data->mutex_time));
+		pthread_mutex_lock(&(data->mutex_end));
+		data->end = 0;
+		pthread_mutex_unlock(&(data->mutex_end));
+		if (set_time_stamp(&(data->phil[i]), &time_from_start))
+			return (FAIL);
+		pthread_mutex_lock(&(data->mutex_print));
+		printf("%lld %d died\n", time_from_start, data->phil[i].phil_id);
+		pthread_mutex_unlock(&(data->mutex_print));
+		return (FAIL);
+	}
+	else
+		pthread_mutex_unlock(&(data->mutex_time));
+	return (SUCCESS);
+}
+
+void	*check_death(void *data)
+{
+	int	i;
+
+	while (1)
+	{
+		i = 0;
+		while (i < ((t_data *)data)->total_phil)
+		{
+			if (check_all_have_eaten((t_data *)data))
+				return (NULL);
+			if (check_time_to_die((t_data *)data, i))
+				return (NULL);
+			i++;
+		}
+	}
 }
