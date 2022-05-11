10a0c564b928be666c47495d5a3386a194e65b4f
AOhapkin <alexeyyago@gmail.com>
2022-05-11 17:01:09 +0300

    rewrite main

--- philo/source/philo.c
+++ philo/source/philo.c
@@ -6,75 +6,101 @@
 /*   By: gmyriah <gmyriah@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 09:36:38 by gmyriah           #+#    #+#             */
-/*   Updated: 2022/05/01 09:36:40 by gmyriah          ###   ########.fr       */
+/*   Updated: 2022/05/11 17:00:57 by gmyriah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	show_philo_message(t_philo *philo, char *message)
+int	set_current_time(long long *time_ms)
 {
-	long long		ms;
-	struct timeval	now;
+	struct timeval		current_time;
 
-	pthread_mutex_lock(&philo->info->finish_mutex);
-	gettimeofday(&now, NULL);
-	ms = time_to_ms(now) - time_to_ms(philo->info->create_at);
-	if (!philo->info->finish)
-		printf("%lld\t%d\t %s\n", ms, philo->n + 1, message);
-	pthread_mutex_unlock(&philo->info->finish_mutex);
+	if (gettimeofday(&current_time, NULL) != 0)
+		return (show_error_message("gettimeofday fail\n"));
+	*time_ms = current_time.tv_sec * 1000 + current_time.tv_usec / 1000;
+	return (0);
 }
 
-static void	join_and_destroy_philos(t_info *info)
+int	show_error_message(char *message)
 {
+	char	*ptr;
 	int		i;
 
+	ptr = message;
 	i = 0;
-	while (i < info->num_of_philo)
-	{
-		pthread_join(info->philos[i].thread, NULL);
-		pthread_mutex_destroy(&info->philos[i++].check_mutex);
-	}
-	free(info->philos);
-	i = 0;
-	while (i < info->num_of_philo)
-		pthread_mutex_destroy(&info->forks[i++]);
-	free(info->forks);
+	while (*ptr++)
+		i++;
+	write(2, message, i);
+	return (FAIL);
 }
 
-static void	create_philos(t_info *info)
+static int	init_threads(t_data *data, pthread_t **philos, pthread_t *monitor)
 {
-	int			i;
-	pthread_t	thread;
+	int	i;
 
-	gettimeofday(&info->create_at, NULL);
+	*philos = malloc(sizeof(pthread_t) * data->total_phil);
+	if (!(*philos))
+		return (show_error_message("malloc error\n"));
+	pthread_mutex_init(&(data->mutex_print), NULL);
+	pthread_mutex_init(&(data->mutex_time), NULL);
+	pthread_mutex_init(&(data->mutex_end), NULL);
+	pthread_mutex_init(&(data->mutex_all_have_eaten), NULL);
+	data->mutex_forks = malloc(sizeof(pthread_mutex_t) * data->total_phil);
+	if (!(data->mutex_forks))
+		return (show_error_message("malloc error\n"));
 	i = 0;
-	while (i < info->num_of_philo)
+	while (i < data->total_phil)
+		pthread_mutex_init(&(data->mutex_forks[i++]), NULL);
+	i = 0;
+	while (i < data->total_phil)
 	{
-		info->philos[i].last_time_to_eat = info->create_at;
-		pthread_create(&info->philos[i].thread, NULL, \
-			run_philo_actions, &info->philos[i]);
-		pthread_create(&thread, NULL, monitor, &info->philos[i]);
-		pthread_detach(thread);
+		if (pthread_create(*(philos) + i, NULL, &actions, &(data->phil[i])) != 0)
+			return (show_error_message("pthread_create fail\n"));
 		i++;
 	}
-	if (info->num_of_must_eat != 0)
+	if (pthread_create(monitor, NULL, &check_death, data) != 0)
+		return (show_error_message("pthread_create fail\n"));
+	return (0);
+}
+
+static int	join_threads(t_data *data, pthread_t **philos, pthread_t *monitor)
+{
+	int	i;
+
+	if (pthread_join(*monitor, NULL) != 0)
+		return (show_error_message("pthread_join fail\n"));
+	i = 0;
+	while (i < data->total_phil)
 	{
-		pthread_create(&thread, NULL, monitor_each_must_eat, info);
-		pthread_detach(thread);
+		if (pthread_join((*philos)[i], (void **) &(data->phil[i])) != 0)
+			return (show_error_message("pthread_join fail\n"));
+		i++;
 	}
+	pthread_mutex_destroy(&(data->mutex_print));
+	pthread_mutex_destroy(&(data->mutex_time));
+	pthread_mutex_destroy(&(data->mutex_end));
+	pthread_mutex_destroy(&(data->mutex_all_have_eaten));
+	i = 0;
+	while (i < data->total_phil)
+		pthread_mutex_destroy(&(data->mutex_forks[i++]));
+	free(*philos);
+	free(data->phil);
+	free(data->mutex_forks);
+	return (SUCCESS);
 }
 
 int	main(int argc, char **argv)
 {
-	t_info	info;
+	t_data		data;
+	pthread_t	*philos;
+	pthread_t	monitor;
 
-	if (argc != 5 && argc != 6)
-		return (show_error_message("Error: wrong arguments\n"));
-	memset(&info, 0, sizeof(info));
-	if (init_simulation(&info, argc, argv))
-		return (1);
-	create_philos(&info);
-	join_and_destroy_philos(&info);
-	return (0);
+	if (handle_input(argc, argv, &data))
+		return (FAIL);
+	if (init_threads(&data, &philos, &monitor))
+		return (FAIL);
+	if (join_threads(&data, &philos, &monitor))
+		return (FAIL);
+	return (SUCCESS);
 }
